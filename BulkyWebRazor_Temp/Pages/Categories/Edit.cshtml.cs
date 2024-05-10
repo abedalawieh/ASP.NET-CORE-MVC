d8eccbe87fc550fd184ab1cc49b931e1eab9c250
abedalaweye@gmail.com <Maramsabra1010>
2024-05-10 22:44:59 +0300

    Added All needed Files

---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,42 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+       
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id!= 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+
+        }
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid) { 
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["update"] = "Category Updated Successfully";
+
+                return RedirectToPage("Index");
+            }
+                
+            return Page();
+            
+
+        }
+    
+}
+}
