c6420a3151b0e8bd36bdcff9c8d2a5d611048b56
abedalaweye@gmail.com <Maramsabra1010>
2024-05-19 13:00:25 +0300

    Added the home page with the functionality and the details page

--- MVCProject/Areas/Customer/Controllers/HomeController.cs
+++ MVCProject/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using MVCProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using MVCProject.DataAccess.Repository.IRepository;
 
 namespace MVCProject.Areas.Customer.Controllers
 
@@ -9,17 +10,27 @@ namespace MVCProject.Areas.Customer.Controllers
 
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products=_unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(products);
         }
+        public IActionResult Details(int? id)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id==id,includeProperties: "Category");
+                return View(product);
+        }
+
+
 
         public IActionResult Privacy()
         {
